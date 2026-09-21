3bc6184129d6fbf8c273d043d2f5df9068a147bd
Користувач <sviatoslavpurshak18@gmail.com>
2026-09-21 21:13:31 +0300

    second comit

--- public/app.js
+++ public/app.js
@@ -36,4 +36,41 @@ async function refresh() {
     }
 }
 
+async function submitProductForm(event) {
+    event.preventDefault();
+
+    const errBlock = document.querySelector('#error');
+    errBlock.textContent = '';
+
+    const formData = new FormData(event.target);
+    const title = formData.get('title');
+    const price = Number(formData.get('price'));
+
+    console.log({ title, price });
+
+    try {
+        const res = await fetch('/api/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title, price })
+        });
+
+        const resData = await res.json();
+
+        if (!res.ok) {
+            throw new Error(resData.error || 'Помилка при додаванні товару');
+        }
+
+        event.target.reset();
+        refresh();
+
+    } catch (err) {
+        errBlock.textContent = err.message;
+    }
+}
+
+document.querySelector('#form').addEventListener('submit', submitProductForm);
+
 refresh();
